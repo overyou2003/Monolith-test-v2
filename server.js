5fd65eb5a701e03c6f50c1d094e7ce86533bf5f7
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-09-30 21:12:09 +0700

    commit from dorm

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ let DOWN_REASON = '';
 
 // ---- Demo data
 const PRODUCTS = [
-  { id: 'p1', name: 'กล้องโพลารอยด์', price: 3990, img: 'https://picsum.photos/seed/camera/800/600' },
-  { id: 'p2', name: 'สติ๊กเกอร์ Pastel', price: 120, img: 'https://picsum.photos/seed/sticker/800/600' },
-  { id: 'p3', name: 'ขาตั้งกล้องเล็ก', price: 590, img: 'https://picsum.photos/seed/tripod/800/600' },
-  { id: 'p4', name: 'ถุงผ้า Lumera', price: 290, img: 'https://picsum.photos/seed/totebag/800/600' }
+  { id: 'p1', name: 'Nike Air Force 1', price: 3000, stock: 8,
+    img: 'https://images.unsplash.com/photo-1600269452121-4f2416e55c28?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 'p2', name: 'Nike Sports Shoes', price: 2500, stock: 120,
+    img: 'https://images.unsplash.com/photo-1460353581641-37baddab0fa2?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 'p3', name: 'Adidas Jersey Shirt', price: 899, stock: 34,
+    img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 'p4', name: 'Adidas Superstar', price: 2900, stock: 50,
+    img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
 ];
 let CART = [];   // [{productId, qty}]
 let ORDERS = []; // [{id, items, amount, time}]
